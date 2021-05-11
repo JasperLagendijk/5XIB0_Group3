e13e2dac2f1eb883b293f0a8286a41ac2e54c486
Jasper Lagendijk <jasperlagendijk@pop-os.localdomain>
2021-05-11 11:27:53 +0200

    Moved functionality from moveTo() to forward() v1

--- robot_base.cpp
+++ robot_base.cpp
@@ -21,14 +21,46 @@ Servo servo_grabber;
 #define WIDTHROBOT 0.104 // In meters
 
 
-void forward() 
+void forward()
 {
   servo_grabber.attach(grabberServo);
   servo_left.attach(servoLeft);
   servo_right.attach(servoRight);
+
+  int left, right;
+  double circumference = 0.21;
+  int prev_left=left;
+  int prev_right=right;
+  double rotations = 0;
+  double rotations_l = 0;
+  double rotations_r = 0;
+  double expectedRotations = distance/circumference;// / CIRCUMFERENCE;
+
   servo_left.writeMicroseconds(1600);
   servo_right.writeMicroseconds(1400);
   servo_grabber.write(20);
+
+  while(rotations < expectedRotations) {
+      left = digitalRead(encoderLeft);
+      right = digitalRead(encoderRight);
+      if(left != prev_left) { //1/8th of rotation made left
+        rotations_l+= (0.0625);
+        //Serial.println(rotations_l);
+      }
+      if(right != prev_right) { //1/8th of rotation made left
+        rotations_r += 0.0625;
+      }
+      //if (abs(rotations_r - rotations_l) > 1) {
+        //ERROR
+      //} else {
+        rotations = (rotations_l+rotations_r)/2;
+      //}
+      prev_left = left;
+      prev_right = right;
+      //Serial.println(rotations);
+  }
+
+
 }
 
 void backward() {
@@ -118,14 +150,7 @@ void moveTo(double x_start, double y_start, double x_end, double y_end, double *
   double dx = x_end-x_start;
   double dy = y_end-y_start;
   double distance = sqrt(pow(dx, 2)+pow(dy, 2));
-  int left, right;
-  double circumference = 0.21;
-  int prev_left=left;
-  int prev_right=right;
-  double rotations = 0;
-  double rotations_l = 0;
-  double rotations_r = 0;
-  double expectedRotations = distance/circumference;// / CIRCUMFERENCE;
+
   //Serial.println(distance);
   //Serial.println(CIRCUMFERENCE);
   //Serial.println(expectedRotations);
@@ -134,24 +159,5 @@ void moveTo(double x_start, double y_start, double x_end, double y_end, double *
   turn(psi, phi);
   forward();
 
-  while(rotations < expectedRotations) {
-      left = digitalRead(encoderLeft);
-      right = digitalRead(encoderRight);
-      if(left != prev_left) { //1/8th of rotation made left
-        rotations_l+= (0.0625);
-        //Serial.println(rotations_l);
-      }
-      if(right != prev_right) { //1/8th of rotation made left
-        rotations_r += 0.0625;
-      }
-      //if (abs(rotations_r - rotations_l) > 1) {
-        //ERROR
-      //} else {
-        rotations = (rotations_l+rotations_r)/2;
-      //}
-      prev_left = left;
-      prev_right = right;
-      //Serial.println(rotations);
-  }
-  brake();
+
 }
